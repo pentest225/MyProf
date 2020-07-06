d754fe11fe1e141a5606b5b3e3c8da16ffefcda2
pentest225 <patrick.dagouaga@uvci.edu.ci>
2020-07-06 17:23:25 +0000

    Dinamisation system de recherche

--- myprofmobil/lib/widgets/matierreCard.dart
+++ myprofmobil/lib/widgets/matierreCard.dart
@@ -8,7 +8,7 @@ class MatierreCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       // height: 20,
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       decoration: BoxDecoration(
           color: Colors.grey.withOpacity(.5),
@@ -16,7 +16,8 @@ class MatierreCard extends StatelessWidget {
       child: Text(
         "${this.matierre}",
         style: TextStyle(
-          fontSize: 25,
+          fontSize: 20,
+          color: Colors.white
         ),
       ),
     );
