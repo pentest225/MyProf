ddbf227b1eaff65096e7e70a59addb372a21aa3a
esthy07 <akissi.ngoran@uvci.edu.ci>
2020-08-08 21:21:17 +0000

    ajout drawer

--- myprofmobil/lib/widgets/myDrower.dart
+++ myprofmobil/lib/widgets/myDrower.dart
@@ -27,195 +27,247 @@ class MyDrower extends StatelessWidget {
 
     return Drawer(
       child: Container(
-          color: Colors.white,
+          color: Colors.transparent,
           child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               Container(
-                padding: EdgeInsets.only(top: deviceHeight / 3.8),
+               // margin: EdgeInsets.only(right: 8),
+                padding: EdgeInsets.only(top: deviceHeight / 3.8,),
                 alignment: Alignment.center,
                 height: deviceHeight,
-                width: deviceWidth / 3.5,
-                color: themeColor.withOpacity(0.9),
+                width: deviceWidth / 3.3,
+                color: Colors.white,
                 child: Column(
                   children: <Widget>[
-                    Icon(Icons.school, size: 80, color: Colors.white,),
+                   InkWell(
+      onTap: () {
+        //Navigator.pushNamed(context, route);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+                  child: Container(
+            height: deviceWidth / 3.5 - 10,
+            width: deviceWidth / 2.5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+                /*boxShadow: [
+                  BoxShadow(
+
+                      color: Color(0x1a000000),
+                      blurRadius: 10,
+                      spreadRadius: 0,
+                      offset: Offset(0, 10)
+
+                      /*color: Colors.black87,
+                      offset: Offset(0.0, 1.5),
+                      blurRadius: 1.5*/
+                  )
+                ]*/
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.school,
+                  color: accanceColor,
+                  size: 80,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
                     Text('Annonce', style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white, fontSize: 22, fontFamily: 'BAARS'
+                      fontWeight: FontWeight.bold, color: Colors.black, fontSize: 22, fontFamily: 'BAARS'
                     ),),
                     Container(
-                      margin: EdgeInsets.only(top: 15),
+                      margin: EdgeInsets.only(top: 15,),
                       child: Text('+', style: TextStyle(
-                       color: Colors.white, fontSize: 40
+                       color: themeColor, fontSize: 40
                         ),)
-                    )
+                    ),
+                       
                   ],
                   ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.topCenter,
-                    margin: EdgeInsets.only(top: deviceHeight / 8, left: deviceWidth / 18),
-                    height: deviceHeight / 8,
-                    width: deviceWidth / 4,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/man.jpg'),
-                        fit: BoxFit.cover
-                      )
+              Container(
+                height: deviceHeight,
+                width: 2,
+                 color: Colors.black45.withOpacity(0.1),
+              ),
+              Container(
+                color: fondcolor,
+                width: deviceWidth /1.87,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topCenter,
+                      margin: EdgeInsets.only(top: deviceHeight / 8, left: deviceWidth / 18),
+                      height: deviceHeight / 8,
+                      width: deviceWidth / 4,
+                      decoration: BoxDecoration(
+                        //color: Colors.white,
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/man.jpg'),
+                          fit: BoxFit.cover
+                        )
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10),
-                    padding: EdgeInsets.only(left: 15),
-                    child: Text('MyProf', style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'BAARS'),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text('MyProf', style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'BAARS'),
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 8),
-                    padding: EdgeInsets.only(left: 15),
-                    child: Text('youss.nan.ci', style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.black54
-                    ),)
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.only(top: 18),
-                    color: Colors.black45.withOpacity(0.1),
-                    width: deviceWidth / 1.8,
-                    height: 2,
-                  ),
-
-                  Container(
-                    margin:  EdgeInsets.only(top: 20),
-                    padding: EdgeInsets.only(left : 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.navigation, color: themeColor, size: 22,) ,
-                            SizedBox(width: 5,),
-                            InkWell(
-                              onTap: (){
-
-                              } ,
-                              child: Text('Navigateur', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              ),),
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 15,),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.person, color: themeColor, size: 22,),
-                            SizedBox(width: 5,),
-                            InkWell(
-                               onTap: (){
-                                  Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) => MainAnnonce()));
-                              } ,
-                              child: Text('Annonce', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              )),
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 15,),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.person, color: themeColor, size: 22,),
-                            SizedBox(width: 5,),
-                            InkWell(
-                               onTap: (){
-                                  Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) => ProfilPage()));
-                              } ,
-                              child: Text('Profil', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              )),
-                            )
-                          ],
-                        ),
-                      ],
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text('youss.nan.ci', style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.black54
+                      ),)
                     ),
-                  ),
 
                     Container(
-                    margin: EdgeInsets.only(top: 18),
-                     color: Colors.black45.withOpacity(0.1),
-                    width: deviceWidth / 1.8,
-                    height: 2,
-                  ),
-
-                  Container(
-                    margin:  EdgeInsets.only(top: 20),
-                    padding: EdgeInsets.only(left : 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.dashboard, color: themeColor, size: 22,),
-                            SizedBox(width: 5,),
-                            InkWell(
-                               onTap: (){
-                                 Navigator.of(context).pushNamed(UserDash.routeName);
-                              } ,
-                              child: Text('Tableau de bord', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              )),
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 15,),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.bookmark, color:themeColor, size: 22,),
-                            SizedBox(width: 5,),
-                            InkWell(
-                               onTap: (){
-                                  Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) => DemandeListe()));
-                              } ,
-                              child: Text('Demande de cours', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              )),
-                            )
-                          ],
-                        ),
-                          SizedBox(height: 15,),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.save_alt, color: themeColor, size: 22,),
-                            SizedBox(width: 5,),
-                            InkWell(
-                               onTap: (){
-                                 Navigator.push(context,
-                                 MaterialPageRoute(builder: (context) => FirstPage()));
-                              } ,
-                              child: Text('Deconnexion', style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
-                              )),
-                            )
-                          ],
-                        ),
-                      ],
+                      margin: EdgeInsets.only(top: 18),
+                      color: Colors.black45.withOpacity(0.1),
+                      width: deviceWidth /1.9,
+                      height: 2,
                     ),
-                  ),
 
-                  Container(
-                    margin: EdgeInsets.only(top: 18),
-                     color: Colors.black45.withOpacity(0.1),
-                    width: deviceWidth / 1.8,
-                    height: 2,
-                  ),
-                ],
+                    Container(
+                      margin:  EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.only(left : 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.navigation, color: themeColor, size: 22,) ,
+                              SizedBox(width: 5,),
+                              InkWell(
+                                onTap: (){
+
+                                } ,
+                                child: Text('Navigateur', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                ),),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.person, color: themeColor, size: 22,),
+                              SizedBox(width: 5,),
+                              InkWell(
+                                 onTap: (){
+                                    Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => MainAnnonce()));
+                                } ,
+                                child: Text('Annonce', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                )),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.person, color: themeColor, size: 22,),
+                              SizedBox(width: 5,),
+                              InkWell(
+                                 onTap: (){
+                                    Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => ProfilPage()));
+                                } ,
+                                child: Text('Profil', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                )),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                      Container(
+                      margin: EdgeInsets.only(top: 18),
+                       color: Colors.black45.withOpacity(0.1),
+                      width: deviceWidth /1.9,
+                      height: 2,
+                    ),
+
+                    Container(
+                      margin:  EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.only(left : 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.dashboard, color: themeColor, size: 22,),
+                              SizedBox(width: 5,),
+                              InkWell(
+                                 onTap: (){
+                                   Navigator.of(context).pushNamed(UserDash.routeName);
+                                } ,
+                                child: Text('Tableau de bord', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                )),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.bookmark, color:themeColor, size: 22,),
+                              SizedBox(width: 5,),
+                              InkWell(
+                                 onTap: (){
+                                    Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => DemandeListe()));
+                                } ,
+                                child: Text('Demande de cours', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                )),
+                              )
+                            ],
+                          ),
+                            SizedBox(height: 15,),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.save_alt, color: themeColor, size: 22,),
+                              SizedBox(width: 5,),
+                              InkWell(
+                                 onTap: (){
+                                   Navigator.push(context,
+                                   MaterialPageRoute(builder: (context) => FirstPage()));
+                                } ,
+                                child: Text('Deconnexion', style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.black87, fontFamily: 'Barlow'
+                                )),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      margin: EdgeInsets.only(top: 18),
+                       color: Colors.black45.withOpacity(0.1),
+                      width: deviceWidth /1.9,
+                      height: 2,
+                    ),
+                  ],
+                ),
               )
             ],
           )
