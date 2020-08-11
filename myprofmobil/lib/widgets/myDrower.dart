19346fef83e76ff34e81cfbc898f48325dbc8d37
esthy07 <akissi.ngoran@uvci.edu.ci>
2020-08-11 16:24:53 +0000

    ajout

--- myprofmobil/lib/widgets/myDrower.dart
+++ myprofmobil/lib/widgets/myDrower.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myprofmobil/outils/myStyle.dart';
 import 'package:myprofmobil/screnn/firstPage.dart';
-import 'package:myprofmobil/screnn/message.dart';
 import 'package:myprofmobil/screnn/home_screen.dart';
 import 'package:myprofmobil/screnn/profil/profil.dart';
 import 'package:myprofmobil/providers/annonces/annonces.dart';
@@ -26,6 +25,7 @@ class MyDrower extends StatelessWidget {
     final annonce = Provider.of<Annonces>(context).items;
 
     return Container(
+      width: deviceWidth / 1.1,
       child: Drawer(
         child: Container(
             child: Row(
@@ -38,24 +38,30 @@ class MyDrower extends StatelessWidget {
                   color: Colors.white,
                   child: ListView(
                     children: <Widget>[
-                      ...annonce.map((annoceOfUser) => InkWell(
-                        onTap: ()=> null,
+                      Container(
+                          child: Column(
+                            children: [
+                              FormSectionTitle("annonce"),
+                              Chip(
+                                label: Text(
+                                  '+',
+                                  style: TextStyle(
+                                      color: Styles.secondaryColor, fontSize: 40),
+                                ),
+                              ),
+                            ],
+                          )),
+                      ...annonce
+                          .map((annoceOfUser) => InkWell(
+                        onTap: () => null,
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Card(
-                              child: Container(
+                            child: Container(
                               height: deviceWidth / 3.7,
                               width: deviceWidth / 2.5,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(20),
-                                  /*boxShadow: [
-                                    BoxShadow(
-                                        color: Color(0x1a000000),
-                                        blurRadius: 10,
-                                        spreadRadius: 0,
-                                        offset: Offset(0, 10)
-                                    )
-                                  ]*/
                               ),
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
@@ -63,196 +69,120 @@ class MyDrower extends StatelessWidget {
                                   Icon(
                                     Icons.school,
                                     color: accanceColor,
-                                    size: 70,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 25),
-              alignment: Alignment.center,
-              height: deviceHeight,
-              width: deviceWidth / 3.3,
-              color: Colors.white,
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                      child: Column(
-                    children: [
-                      FormSectionTitle("annonce"),
-                      Chip(
-                        label: Text(
-                          '+',
-                          style: TextStyle(
-                              color: Styles.secondaryColor, fontSize: 40),
-                        ),
-                      ),
-                    ],
-                  )),
-                  ...annonce
-                      .map((annoceOfUser) => InkWell(
-                            onTap: () => null,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Card(
-                                child: Container(
-                                  height: deviceWidth / 3.7,
-                                  width: deviceWidth / 2.5,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
+                                    size: 80,
                                   ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.school,
-                                        color: accanceColor,
-                                        size: 80,
-                                      ),
-                                      Text(annoceOfUser.fields.titre
-                                          .substring(0, 10))
-                                    ],
-                                  ),
-                                ),
+                                  Text(annoceOfUser.fields.titre
+                                      .substring(0, 10))
+                                ],
                               ),
                             ),
-
                           ),
                         ),
-                      )).take(3).toList(),
-                      Container(
-                          child: Column(
-                            children: [
-                              FormSectionTitle("annonce"),
-                              Chip(
-                                label: Text('+', style: TextStyle(
-                                    color: Styles.secondaryColor, fontSize: 40
-                                ),),
-                              ),
-                            ],
-                          )
-                      ),
-                    ],),
+                      ))
+                          .take(3)
+                          .toList(),
+                    ],
+                  ),
                 ),
                 Container(
                   height: deviceHeight,
                   width: 2,
-                   color: Colors.black45.withOpacity(0.1),
+                  color: Colors.black45.withOpacity(0.1),
                 ),
-                Container(
-                  color: fondcolor,
-                  width: deviceWidth / 1.87,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                       SizedBox(height: 35,),
-                      Column(
-                        children: [
-
-                          ))
-                      .take(3)
-                      .toList(),
-                ],
-              ),
-            ),
-            Container(
-              height: deviceHeight,
-              width: 2,
-              color: Colors.black45.withOpacity(0.1),
-            ),
-            //Deuxiem partie du drower 
-            Expanded(
-              child: Container(
-                color: fondcolor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 35,
-                    ),
-                    Column(
-                      children: [
-
-                        Container(
-                          alignment: Alignment.center,
-                          height: deviceHeight / 8,
-                          width: deviceWidth / 4,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  image: AssetImage('assets/images/man.jpg'),
-                                  fit: BoxFit.cover)),
+                //Deuxiem partie du drower
+                Expanded(
+                  child: Container(
+                    color: fondcolor,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 35,
                         ),
-                        SizedBox(height: 7),
+                        Column(
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              height: deviceHeight / 8,
+                              width: deviceWidth / 4,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/images/man.jpg'),
+                                      fit: BoxFit.cover)),
+                            ),
+                            SizedBox(height: 7),
+                            Container(
+                              child: Text(
+                                'MyProf',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'BAARS'),
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                                child: Text(
+                                  'youss.nan.ci',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black54),
+                                )),
+                            Separator(),
+                          ],
+                        ),
                         Container(
-                          child: Text(
-                            'MyProf',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'BAARS'),
+                          padding: EdgeInsets.only(left: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              MyRoute(text: "Navigator",icon: Icons.navigation,route: HomeScreen.rooteName,),
+                              // Spacer(),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              MyRoute(text: "Annonce",icon: Icons.add,route: MainAnnonce.routeName,),
+                              // Spacer(),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              MyRoute(text: "Profil",icon: Icons.person,route: ProfilPage.routeName,),
+
+                            ],
                           ),
                         ),
-                        SizedBox(height: 5),
-                        Container(
-                            child: Text(
-                          'youss.nan.ci',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black54),
-                        )),
                         Separator(),
+                        Container(
+                          padding: EdgeInsets.only(left: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              MyRoute(text: "Tableau de bord",icon: Icons.dashboard,route: UserDash.routeName,),
+
+                              SizedBox(
+                                height: 15,
+                              ),
+
+                              MyRoute(text: "Demande de cours",icon: Icons.bookmark,route: DemandeListe.routeName,),
+
+                              SizedBox(
+                                height: 15,
+                              ),
+                              MyRoute(text: "Deconnexion",icon: Icons.save_alt,route: FirstPage.routeName,),
+
+                            ],
+                          ),
+                        ),
+                        Spacer()
                       ],
                     ),
-                    Container(
-                      padding: EdgeInsets.only(left: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          MyRoute(text: "Navigator",icon: Icons.navigation,route: HomeScreen.rooteName,),
-                          // Spacer(),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          MyRoute(text: "Annonce",icon: Icons.add,route: MainAnnonce.routeName,),
-                          // Spacer(),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          MyRoute(text: "Profil",icon: Icons.person,route: ProfilPage.routeName,),
-                          
-                        ],
-                      ),
-                    ),
-                    Separator(),
-                    Container(
-                      padding: EdgeInsets.only(left: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          MyRoute(text: "Tableau de bord",icon: Icons.dashboard,route: UserDash.routeName,),
-                       
-                          SizedBox(
-                            height: 15,
-                          ),
-
-                           MyRoute(text: "Demande de cours",icon: Icons.bookmark,route: DemandeListe.routeName,),
-                          
-                          SizedBox(
-                            height: 15,
-                          ),
-                          MyRoute(text: "Deconnexion",icon: Icons.save_alt,route: FirstPage.routeName,),
-                          
-                        ],
-                      ),
-                    ),
-                    Spacer()
-                  ],
-                ),
-              ),
-            )
-          ],
-        )),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
